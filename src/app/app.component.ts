92916c507775ad0ca273db20ac11765d3bdb0508
calvin <exe985632@gmail.com>
2023-08-16 12:44:53 +0800

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'timezone';
+
+  ngOnInit() {
+    console.log('test');
+    console.log(process.env);
+  }
 }
